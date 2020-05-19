66d0eb5042397ace5e6619f6ca788f8a1b8aa396
Samuel Baker <65611363+samuelbaker93@users.noreply.github.com>
2020-05-19 16:45:36 +0100

    Add files via upload

---
+++ graphy/__init__.py
@@ -0,0 +1,13 @@
+# Set name spaces for external libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import other libraries
+import sys
+
+# Import graphy Objects
+from graphy.core import *
+from graphy.styles import *
+
+__version__ = "0.02.0"
