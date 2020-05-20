4b6e450742e00a5163baed5bb0aebc8cb8c00154
Samuel Baker <65611363+sbaker-dev@users.noreply.github.com>
2020-05-20 02:50:43 +0100

    Add files via upload

---
+++ graphy2/api.py
@@ -0,0 +1,28 @@
+from graphy2 import sys
+from graphy2.core import Graphy
+
+if __name__ == '__main__':
+    # Rgraphy2 Null args don't get passed as system arguments, but False will be passed as a string rather than as a
+    # bool so args needs formatting so that a False String returns a None type
+    args = []
+    for arg in sys.argv:
+        if "FALSE" in arg:
+            args.append(None)
+        else:
+            args.append(arg)
+
+    try:
+        function_name = sys.argv[1]
+    except IndexError as e:
+        raise Exception("No arguments passed to graphy_call.py") from e
+
+    try:
+        class_args = [sys.argv[i] for i in range(2, 6)]
+    except IndexError as e:
+        raise Exception("Not enough arguments passed to graphy_call.py for the class args. Args should be:\n"
+                        "[0]Script_name\n[1]class_method_name\n[2]path_to_data_file\n[3]write_directory\n"
+                        "[4]figure_name\n[5]style_sheet\n[6+]args for class_method_name")
+
+    method_args = [sys.argv[i] for i in range(6, len(sys.argv))]
+
+    getattr(Graphy(*class_args), str(function_name))(*method_args)
