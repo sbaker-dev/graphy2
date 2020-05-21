f334d0b2b50e28bf86d0d320a36d84c93c00d45a
Nina Di Cara <ninadicara@gmail.com>
2020-05-21 23:10:20 +0100

    Add example files for new plots

---
+++ Examples/python_boxplot_example.py
@@ -0,0 +1,18 @@
+"""
+This is a basic example of how to use the library to create a boxplot in Python
+"""
+from graphy2.core import Graphy
+
+if __name__ == "__main__":
+
+    import seaborn as sns
+    import os
+
+    # Load an example dataset from seaborn as the sample dataset
+    data = sns.load_dataset("tips")
+
+    # Get the path for the directory of this file, and save output to new sub-directory 'plots'
+    write_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plots")
+
+    # Call graphy2 to produce a box plot
+    Graphy(data, write_dir).box_plot(x_var="day", y_var="total_bill")
