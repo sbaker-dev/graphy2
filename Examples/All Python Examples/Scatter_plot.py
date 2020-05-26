95a431655fcea24afeafcdd42b9dd76dddc548c1
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-05-26 10:14:57 +0100

    Making consistent with others

--- Examples/All Python Examples/Scatter_plot.py
+++ Examples/All Python Examples/Scatter_plot.py
@@ -2,12 +2,19 @@
 This is a basic example of how to use the library natively in python.
 """
 from graphy2.core import Graphy
+import os
+import seaborn as sns
 
 if __name__ == "__main__":
-    write_dir = r"C:\Users\Samuel\Pictures\Graphy"
-    stylesheet = {"figure_x": 10}
-    csv_path = r"C:\Users\Samuel\Pictures\Graphy\Diamonds.csv"
 
+    # Load data set
+    diamonds = sns.load_dataset("diamonds")
+
+    # Get the path for the directory of this file, and save output to new sub-directory 'plots'
+    write_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plots")
+
+    # Use a custom ranking relating to the clarity within the data
     clarity_ranking = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
 
-    Graphy(csv_path, write_dir).scatter_plot("carat", "price", "clarity", "depth")
+    # Call graphy2 to produce the scatter plot
+    Graphy(diamonds, write_dir, "Scatter Plot").scatter_plot("carat", "price", "clarity", "depth", clarity_ranking)
