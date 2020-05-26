b24aa64acc33b4e723b016aceb09fd9275ec008e
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-05-26 12:49:52 +0100

    Add some style sheets to examples

--- Examples/All Python Examples/Boxplot_example.py
+++ Examples/All Python Examples/Boxplot_example.py
@@ -2,6 +2,7 @@
 This is a basic example of how to use the library to create a boxplot in Python
 """
 from graphy2.core import Graphy
+from graphy2.style_sheets import BLUE_ON_WHITE_GRID
 import seaborn as sns
 import os
 
@@ -13,5 +14,8 @@ if __name__ == "__main__":
     # Get the path for the directory of this file, and save output to new sub-directory 'plots'
     write_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plots")
 
+    # Modify a default style to have a smaller image size for a Non-print online only figure
+    BLUE_ON_WHITE_GRID["dpi"] = 72
+
     # Call graphy2 to produce a box plot
-    Graphy(data, write_dir, "Box Plot").box_plot(x_var="day", y_var="total_bill")
+    Graphy(data, write_dir, "Box Plot", BLUE_ON_WHITE_GRID).box_plot(x_var="day", y_var="total_bill")
--- Examples/All Python Examples/Scatter_plot.py
+++ Examples/All Python Examples/Scatter_plot.py
@@ -4,7 +4,7 @@ This is a basic example of how to use the library natively in python.
 from graphy2.core import Graphy
 import os
 import seaborn as sns
-import graphy2.style_sheets as styles
+from graphy2.style_sheets import COOL_ON_WHITE
 
 if __name__ == "__main__":
 
@@ -17,6 +17,9 @@ if __name__ == "__main__":
     # Use a custom ranking relating to the clarity within the data
     clarity_ranking = ["I1", "SI2", "SI1", "VS2", "VS1", "VVS2", "VVS1", "IF"]
 
+    # Modify a prebuilt style to have a wider graph
+    COOL_ON_WHITE["figure_x"] = 8
+
     # # Call graphy2 to produce the scatter plot
-    Graphy(diamonds, write_dir, "Scatter Plot", styles.COOL_ON_WHITE).scatter_plot(
-        "carat", "price", "clarity", "depth", clarity_ranking)
+    Graphy(diamonds, write_dir, "Scatter Plot", COOL_ON_WHITE).scatter_plot("carat", "price", "clarity", "depth",
+                                                                            clarity_ranking)
